2e0b82547208604502bb84f11f6304a8d620e8c7
jeampier <pearjeam@gmail.com>
2026-05-27 11:07:00 -0500

    Add startup script for database migrations and app start

---
+++ startup.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+npm run migrate
+npm run migrate:admin
+npm run migrate:employees
+npm run migrate:smmlv
+npm run migrate:hours
+npm run migrate:absence-types
+npm run migrate:absence-catalog
+npm run migrate:schedule-import
+npm run migrate:rate-rules
+npm run migrate:shift-ordinary
+npm run migrate:requests
+npm start
